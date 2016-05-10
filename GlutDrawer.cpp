27b483a89b587edea41e5dba18f822ed12f2bd4d
lokotochek <lokotochek@gmail.com>
2016-05-11 02:06:03 +0300

    cleaning

--- GlutDrawer.cpp
+++ GlutDrawer.cpp
@@ -5,47 +5,42 @@
 #include "GlutDrawer.h"
 
 void GlutDrawer::initGL() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-    glClearDepth(1.0f);                   // Set background depth to farthest
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearDepth(1.0f);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 void GlutDrawer::display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
 
-    // Render a color-cube consisting of 6 quads with different colors
-    glLoadIdentity();                 // Reset the model-view matrix
-    glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
+    glLoadIdentity();
+    glTranslatef(1.5f, 0.0f, -7.0f);
 
-    /// drawing
+    /// drawing the figures ////////////////////////////////////////////////////////////////////////////////////////////
 
-    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+    glutSwapBuffers();
 }
 
-void GlutDrawer::reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-    // Compute aspect ratio of the new window
-    if (height == 0) height = 1;                // To prevent divide by 0
+void GlutDrawer::reshape(GLsizei width, GLsizei height) {
+    if (height == 0) height = 1;
     GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-    // Set the viewport to cover the new window
     glViewport(0, 0, width, height);
 
-    // Set the aspect ratio of the clipping volume to match the viewport
-    glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-    glLoadIdentity();             // Reset
-    // Enable perspective projection with fovy, aspect, zNear and zFar
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
     gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
 
 void GlutDrawer::startDraw(int &argc, char** argv) {
-    glutInit(&argc, argv);            // Initialize GLUT
-    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-    glutInitWindowSize(800, 800);   // Set the window's initial width & height
-    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-    glutCreateWindow(title);          // Create window with the given title
-    glutDisplayFunc(GlutDrawer::display);       // Register callback handler for window re-paint event
-    glutReshapeFunc(GlutDrawer::reshape);       // Register callback handler for window re-size event
-    initGL();                       // Our own OpenGL initialization
-    glutMainLoop();                 // Enter the infinite event-processing loop
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow(title);
+    glutDisplayFunc(GlutDrawer::display);
+    glutReshapeFunc(GlutDrawer::reshape);
+    initGL();
+    glutMainLoop();
 }
