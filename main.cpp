78d516cdbb9738d09177b18564784ad947bfd36b
lokotochek <lokotochek@gmail.com>
2016-05-11 02:18:01 +0300

    test sphere intersection

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
+//#include <iostream>
+//#include "GlutDrawer.h"
+//
+//int main(int argc, char** argv) {
+//
+//    GlutDrawer gd;
+//    gd.startDraw(argc, argv);
+//    return 0;
+//
+//}
+
 #include <iostream>
-#include "GlutDrawer.h"
+#include "CSphere.h"
 
-int main(int argc, char** argv) {
+int main() {
 
-    GlutDrawer gd;
-    gd.startDraw(argc, argv);
+    CSphere sphere;
+    CVector3D v(CPoint3D(), CPoint3D(5.0, 5.0, 5.0));
+    CPoint3D p;
+
+    if(sphere.intersect(v, p)) {
+        std::cout
+        << "yes: ("
+        << p.get_x()
+        << ", "
+        << p.get_y()
+        << ", "
+        << p.get_z()
+        << ")"
+        << std::endl;
+    }
+
+    else std::cout << "no";
     return 0;
-
 }
