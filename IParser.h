03aa9baa666538f31e5e450dbd36cb907439e86e
lokotochek <lokotochek@gmail.com>
2016-05-11 03:03:51 +0300

    iparser correct

---
+++ IParser.h
@@ -0,0 +1,22 @@
+//
+// Created by Lokotochek on 11.05.16.
+//
+
+#ifndef RAYTRACINGGL_IPARSER_H
+#define RAYTRACINGGL_IPARSER_H
+
+#include "iobject3d.h"
+#include "ccamera.hpp"
+#include "clighter.hpp"
+
+class IParser {
+	public:
+		virtual void parse(const std::string& filename) = 0;
+		virtual std::vector<IObject3D*>& get_objects() = 0;
+		virtual CCamera get_camera() const = 0; 
+		virtual std::vector<CLighter>& get_lighters() = 0;
+		virtual ~IParser() {}
+};
+
+
+#endif //RAYTRACINGGL_IPARSER_H
